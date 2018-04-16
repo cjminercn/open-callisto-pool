4b16509334209d3ff89677e484cb6c684dd423ac
Akira Takizawa <akx20000@protonmail.com>
2018-04-16 18:32:51 +0900

    clopool.net

--- www/config/environment.js
+++ www/config/environment.js
@@ -15,18 +15,18 @@ module.exports = function(environment) {
 
     APP: {
       // API host and port
-      ApiUrl: '//example.net/',
+      ApiUrl: '//clopool.net/',
 
       // HTTP mining endpoint
-      HttpHost: 'http://example.net',
+      HttpHost: 'http://clopool.net',
       HttpPort: 8888,
 
       // Stratum mining endpoint
-      StratumHost: 'example.net',
+      StratumHost: 'clopool.net',
       StratumPort: 8008,
 
       // Fee and payout details
-      PoolFee: '1%',
+      PoolFee: '0%',
       PayoutThreshold: '0.5 CLO',
 
       // For network hashrate (change for your favourite fork)
